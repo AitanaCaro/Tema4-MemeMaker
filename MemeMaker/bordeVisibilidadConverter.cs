339c899376198e3926f392d11bb9c425d36b51b9
AitanaCaro <aitana.caro@iesdoctorbalmis.com>
2022-10-27 12:51:28 +0200

    bindings

---
+++ MemeMaker/bordeVisibilidadConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MemeMaker
+{
+    class BordeVisibilidadConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value) return new System.Windows.Thickness(3.0);
+            else return new System.Windows.Thickness(0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
